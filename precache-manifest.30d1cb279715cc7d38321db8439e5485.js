589ddd4b86abe50d02b7f06a56ec88e007fab711
Jaime Munoz <jmunoz@decibelinsight.com>
2020-12-09 08:39:04 +0100

    Updates

---
+++ precache-manifest.30d1cb279715cc7d38321db8439e5485.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afe5e92e733b2717e9428e027adc0ec3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "252fd47fdad487f633d7",
+    "url": "/static/css/main.917f04b4.chunk.css"
+  },
+  {
+    "revision": "f3bbdd3a1a31866a873c",
+    "url": "/static/js/2.783d6c2d.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/static/js/2.783d6c2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "252fd47fdad487f633d7",
+    "url": "/static/js/main.012dc7c4.chunk.js"
+  },
+  {
+    "revision": "f35af6f7ccead2fe5dbb",
+    "url": "/static/js/runtime-main.4422d9b2.js"
+  }
+]);
